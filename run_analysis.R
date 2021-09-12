e6e73a1b35688db1ebd1433aaf36f4c23cbf7d02
yuliakravchenko <gauri.julia@gmail.com>
2021-09-12 00:51:47 -0400

    added steps 0-2

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,17 @@
 # My script
+
+#Step 0 - Reading the training and test datasets
+traindata <- read.table("./UCI HAR Dataset/train/X_train.txt", header = FALSE)
+trainlabels <- read.table("./UCI HAR Dataset/train/y_train.txt", header = FALSE)
+
+testdata <- read.table("./UCI HAR Dataset/test/X_test.txt", header = FALSE)
+testlabels <- read.table("./UCI HAR Dataset/test/y_test.txt", header = FALSE)
+
+#Step 1 - Merges the training and the test sets to create one data set.
+data <- rbind(traindata, testdata)
+labels <- rbind(trainlabels, testlabels)
+
+#Step 2 - Extracts only the measurements on the mean and standard deviation for each measurement.
+#Figure out measurement names.
+features <- read.table("./UCI HAR Dataset/features.txt", header = FALSE)
+data2 <- data[, grep("-mean|-std", features$V2)]
