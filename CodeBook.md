182a9cc92ae1c45ff16ab1c13caa62b4df91b565
yuliakravchenko <gauri.julia@gmail.com>
2021-09-16 00:48:51 -0400

    All project steps in file run_analysis.R Their description in CodeBook.md

--- CodeBook.md
+++ CodeBook.md
@@ -1,20 +1,40 @@
 ---
-title: "CodeBook"
-author: "me"
-date: "9/11/2021"
-output: html_document
+title: "Code Book"
+---
+## Description of the data modification
+
+The original data was stored in the project folder. 
+The R script stored in run_analysis.R modifies the original data in the following 5 steps:
+
+1. The training and the test sets are merged to create one data set.
+
+2. Only the measurements on the mean and standard deviation for each measurement are extracted.
+According to the description in features_info.txt only features, whoes names ended in -mean() or -std() were selected. So, only 66 measurements from the original list remained.
+
+3. Descriptive activity names are used to name the activities in the data set
+Activities were used as factors. Labels were taken from the activity_labels.txt
+
+4. The data set is appropriately labeled with descriptive variable names. 
+Complete words were used instead of their abbreviations.
+
+5. From the data set in step 4, is created a second, independent tidy data set with the average of each variable for each activity and each subject.
+
 ---
 
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
-```
+## Description of the data fields in tidy.txt
 
-## R Markdown
+### Identifiers
+* Subjects - ID of the test subject (one of the 30 volunteers performing activities)
+* Activities - The type of physical activity performed by the subject.
 
-This is an R Markdown document. Markdown is a simple formatting syntax for authoring HTML, PDF, and MS Word documents. For more details on using R Markdown see <http://rmarkdown.rstudio.com>.
+### Activity Names
+1. WALKING: the subject was walking during the test
+2. WALKING_UPSTAIRS: the subject was walking up stairs during the test
+3. WALKING_DOWNSTAIRS: the subject was walking down stairs during the test
+4. SITTING: the subject was sitting during the test
+5. STANDING: the subject was standing during the test
+6. LAYING: the subject was laying down during the test
 
-When you click the **Knit** button a document will be generated that includes both content as well as the output of any embedded R code chunks within the document. You can embed an R code chunk like this:
-
-```{r cars}
-summary(cars)
-```
+### Features
+These are actual measurements, performed during the activities
+On Step 2 only the measurements on the mean and standard deviation for each measurement were extracted.
--- run_analysis.R
+++ run_analysis.R
@@ -1,17 +1,61 @@
-# My script
+#Step 0 
 
-#Step 0 - Reading the training and test datasets
-traindata <- read.table("./UCI HAR Dataset/train/X_train.txt", header = FALSE)
-trainlabels <- read.table("./UCI HAR Dataset/train/y_train.txt", header = FALSE)
+#Reading features - measurements names
+features <- read.table("./UCI HAR Dataset/features.txt", header = FALSE)
+features[,2] <- as.character(features[,2])
 
-testdata <- read.table("./UCI HAR Dataset/test/X_test.txt", header = FALSE)
-testlabels <- read.table("./UCI HAR Dataset/test/y_test.txt", header = FALSE)
+#Reading activities
+activities <- read.table("./UCI HAR Dataset/activity_labels.txt", header = FALSE)
+activities[,2] <- as.character(activities[,2])
+
+#Reading the training dataset
+trainData <- read.table("./UCI HAR Dataset/train/X_train.txt", header = FALSE)
+trainLabels <- read.table("./UCI HAR Dataset/train/y_train.txt", col.name="Activity", header = FALSE)
+trainSubjects <- read.table("./UCI HAR Dataset/train/subject_train.txt", col.name="Subject", header = FALSE)
+
+#Reading the test dataset
+testData <- read.table("./UCI HAR Dataset/test/X_test.txt", header = FALSE)
+testLabels <- read.table("./UCI HAR Dataset/test/y_test.txt", col.name="Activity", header = FALSE)
+testSubjects <- read.table("./UCI HAR Dataset/test/subject_test.txt", col.name="Subject", header = FALSE)
+
 
 #Step 1 - Merges the training and the test sets to create one data set.
-data <- rbind(traindata, testdata)
-labels <- rbind(trainlabels, testlabels)
+train <- cbind(trainSubjects, trainLabels, trainData) # Binding subjects and activity labels with train data. 
+test <- cbind(testSubjects, testLabels, testData) # Binding subjects and activity labels with test data. 
+fullData <- rbind(train, test) # Binding train and test datasets
+
 
 #Step 2 - Extracts only the measurements on the mean and standard deviation for each measurement.
-#Figure out measurement names.
-features <- read.table("./UCI HAR Dataset/features.txt", header = FALSE)
-data2 <- data[, grep("-mean|-std", features$V2)]
+requiredFeatures <- grep("-mean\\(|-std\\(", features$V2)
+narrowData <- fullData[, c(1,2, requiredFeatures+2)] #Extract only the measurements, described as "mean" and "standard" deviation
+
+
+#Step 3 - Uses descriptive activity names, stored in activities, to name the activities in the data set
+narrowData$Activity = factor(narrowData$Activity, labels=activities[,2])
+
+
+#Step 4 - Appropriately labels the data set with descriptive variable names. 
+requiredFeaturesNames <- features[requiredFeatures,2]
+requiredFeaturesNames <- gsub("[(][])]", "", requiredFeaturesNames) #Getting rid of "()"
+
+#Making feature names more readable
+requiredFeaturesNames <- gsub("^t", "Time Domain. ", requiredFeaturesNames)
+requiredFeaturesNames <- gsub("^f", "Frequency Domain. ", requiredFeaturesNames)
+requiredFeaturesNames <- gsub("-mean", "- Mean ", requiredFeaturesNames)
+requiredFeaturesNames <- gsub("-std", "- Standard Deviation ", requiredFeaturesNames)
+requiredFeaturesNames <- gsub("-X", "- in the X direction", requiredFeaturesNames)
+requiredFeaturesNames <- gsub("-Y", "- in the Y direction", requiredFeaturesNames)
+requiredFeaturesNames <- gsub("-Z", "- in the Z direction", requiredFeaturesNames)
+requiredFeaturesNames <- gsub("BodyAcc", "Body Acceleration ", requiredFeaturesNames)
+requiredFeaturesNames <- gsub("BodyGyro", "Body Gyroscope ", requiredFeaturesNames)
+requiredFeaturesNames <- gsub("GravityAcc", "Gravity Acceleration signal ", requiredFeaturesNames)
+requiredFeaturesNames <- gsub("Mag", " Magnitude ", requiredFeaturesNames)
+requiredFeaturesNames <- gsub("BodyBody", "Body Body ", requiredFeaturesNames)
+
+#Applying new feature names to the new data set.
+names(narrowData) <- c("Subject", "Activity", requiredFeaturesNames)
+
+
+#Step 5 - From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+tidyData <- aggregate(narrowData[,3:68], by = list(Subject = narrowData$Subject, Activity = narrowData$Activity),FUN = mean)
+write.table(tidyData, file = "tidy_data.txt", row.names = FALSE)
